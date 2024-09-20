2d316bc4d5587c305f0b60950a04768056172d88
shaohggaiz <shohaggazi0331@gmail.com>
2024-09-20 19:43:40 +0600

    calculator

---
+++ calculatorAge.js
@@ -0,0 +1,25 @@
+const inputValue = document.querySelector('.date-value');
+const btnAge = document.querySelector('.btn-age');
+const resutl = document.querySelector('.resutl');
+
+resutl.innerHTML = `Your age is 21 years old`;
+
+console.log(inputValue)
+
+
+
+btnAge.addEventListener('click', function() {
+    if(inputValue.value === '') {
+        alert('Please enter date of birth');
+        
+    } else {
+        const dob = new Date(inputValue.value);
+        const dob_year = dob.getFullYear();
+        //current
+        const now = new Date();
+        const new_year = now.getFullYear()
+
+        const age = new_year - dob_year;
+        resutl.innerHTML = `Your age is ${age} Years old`;
+    }
+})
